41a2e7e32e46c80fd96ec2ad7dfcc630436177ab
Rhea Kak <rkk66@cornell.edu>
2022-07-21 23:00:05 -0400

    added secrets template

---
+++ secrets_template.py
@@ -0,0 +1,11 @@
+# spotify username
+spotify_user_id = ""
+
+# navigate to playlist, select "copy playlist uri"
+discover_weekly_playlist_id = ""
+
+# retrieve by following authorization protocol: https://developer.spotify.com/documentation/general/guides/authorization/code-flow/
+refresh_token = ""
+
+# base-64 encoded value of <clientID:clientSecret>
+base_64_encoded_ids = ""
